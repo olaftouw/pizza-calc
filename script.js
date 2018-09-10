e4ee0a6ef732393e97115ed2bd963ffb4cc56f06
Olaf Touw <olaftouw@gmail.com>
2018-09-10 10:15:54 +0100

    update price 1.2

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ var largeprice = large*LPRICE;
 var totalprice = smallprice+mediumprice+largeprice;		//hier word het totaal bedrag uitgerekent.
 
 /* hier komt het antwoord te staan van hoeveel pizzas u heeft gekozen hij haalt het antwoord uit de prompt en de prijs uit de const */
-document.write("U heeft gekozen voor"," ", small," ", "small pizza(s).", " ","€", small*SPRICE, ",-");
+document.write("U heeft gekozen voor"," ", small," ", "small pizza(s).", " ","€", smallprice, ",-");
 document.write("<br><br>");		//deze "<br><br>" staat voor een dubble ENTER.
-document.write("U heeft gekozen voor"," ", medium," ", "medium pizza(s).", " ","€", medium*MPRICE, ",-");
+document.write("U heeft gekozen voor"," ", medium," ", "medium pizza(s).", " ","€", mediumprice, ",-");
 document.write("<br><br>");
-document.write("U heeft gekozen voor"," ", large," ", "large pizza(s).", " ","€", large*LPRICE, ",-");
+document.write("U heeft gekozen voor"," ", large," ", "large pizza(s).", " ","€", largeprice, ",-");
 document.write("<br><br>", "Het totaal bedrag is €", totalprice,",-")
